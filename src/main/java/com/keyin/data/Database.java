81e07ad7325cf9a46e75cb65f74bdab0bcff01d3
TyEngle <95366088+TyEngle@users.noreply.github.com>
2022-10-25 10:28:45 -0230

    Following the code from jamie's lecture and comparing with our own, I added some light-structure and imports to the test file and added a database file with the idea of accessing all members in tournaments

---
+++ src/main/java/com/keyin/data/Database.java
@@ -0,0 +1,18 @@
+package com.keyin.data;
+
+import com.keyin.members.Member;
+import com.keyin.tournaments.Tournament;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Database {
+
+    public Tournament getTournamentByName(String tournamentName){
+        return null;
+    }
+
+    public List<Member> getAllMembers(){
+        return new ArrayList<Member>();
+    }
+}
--- src/test/java/com/keyin/members/TournamentMembersTest.java
+++ src/test/java/com/keyin/members/TournamentMembersTest.java
@@ -1,10 +1,25 @@
 package com.keyin.members;
 
+import com.keyin.data.Database;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class TournamentMembersTest {
+    @Mock
+    private Database mockDatabase;
 
-    public static void main(String[] args){
+//    @Test
+//    public void test<Method name goes here once created>(){}
 
-        // To be used for unit testing
 
-    }
+
+
+//    public static void main(String[] args){
+//
+//        // To be used for unit testing
+//
+//    }
 }
