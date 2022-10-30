5818310dab8be539236c856e655997ce15035d2f
TyEngle <95366088+TyEngle@users.noreply.github.com>
2022-10-30 19:59:53 -0230

    updates to the cli file

--- src/main/java/com/keyin/data/Database.java
+++ src/main/java/com/keyin/data/Database.java
@@ -1,4 +1,8 @@
-// Database file
+//Last edited Oct 30 7:30pm
+//Created for
+//Created on Oct 23
+//Created by Noah Gosse
+
 
 package com.keyin.data;
 import java.util.*;
@@ -6,19 +10,31 @@ import com.keyin.members.Member;
 import com.keyin.members.Tournament;
 public class Database {
     public Member getMemberByName(String name){
-        //returns a member object based on a string sent to it;
+    //parameter references name of member you are searching for
+    //access a database and search for member based on parameter(name)
+    //returns all information available for that member;
+        //mockDatabase.getMemberByName(name).thenReturn(Member member);
+
+
+
         return null;
     }
 
     public List<Member> getMemberByTournamentName(Member member){
+    //parameter
+    //returns
         return null;
     }
 
-    public Tournament getTournamentByName(String name){
+    public Tournament getTournamentByName(String tournamentName){
+    //parameter
+    //returns
         return null;
     }
 
     public List<Tournament> getTournamentByMemberName(Tournament tournament){
+    //parameter
+    //returns
         return null;
     }
 }
--- src/main/java/com/keyin/members/CLI.java
+++ src/main/java/com/keyin/members/CLI.java
@@ -1,16 +1,19 @@
+//Last edited Oct 30 7:30pm
+//Created for
+//Created on Oct 23
+//Created by Noah Gosse
+
 //edit made Oct 30 in AM by T Engle
-// Worked on by Noah Gosse
+
 
 
 package com.keyin.members;
 
 import java.util.Scanner;
-import picocli.CommandLine;
-import picocli.CommandLine.Command;
 
 
 public class CLI {
-    public static int mainMenuCall(){
+    public static int mainMenu(){
     //method to display the main menu of our program
     //returns variable to store menu selection
         int numSelection = 0;
@@ -29,32 +32,98 @@ public class CLI {
 
         return numSelection;
     }
+
+    public static int memberMenu(){
+        //scanner
+        //selection variable
+        //menu options shown with print statements
+        int numSelection;
+
+        Scanner input = new Scanner( System.in );
+
+        System.out.println("Members menu!");
+        System.out.println("1. All users");
+        System.out.println("2. User search");
+        System.out.println("4. Return");
+        numSelection = input.nextInt();
+
+        return numSelection;
+
+    }
+
     public static void main(String[] args){
     //main executable file
         Scanner input = new Scanner( System.in );
-
+    //switch statement is how menu naviation happens
         System.out.println("select an option from the menu: ");
-
-        int numSelection = mainMenuCall();
+        int numSelection = mainMenu();
             switch (numSelection) {
                 case 1:
+                //case1 is members menu
                     System.out.println("Members!");
                     //membersFunctionCall();
+                    //scanner
+                    //selection variable
+                    //menu options shown with print statements
+                    int membersNumSelection = 0;
+
+                    Scanner membersMenuInput = new Scanner( System.in );
+                    while (membersNumSelection != 3) {
+
+
+                        System.out.println("Members menu!");
+                        System.out.println("1. All users");
+                        System.out.println("2. User search");
+                        System.out.println("3. Return");
+                        System.out.println("Make a selection: ");
+                        membersNumSelection = input.nextInt();
+                        if (membersNumSelection == 1) {
+                            //return all users stored in the database
+
+                        } else if (membersNumSelection == 2) {
+                            System.out.println("Enter name of member you are requesting info for: ");
+
+                        } else if (membersNumSelection == 3) {
+                            mainMenu();
+                        } else {
+                            System.out.println("Sorry wrong input!");
+                        }
+                    }
                     break;
                 case 2:
+                //case2 is tournaments menu
                     System.out.println("Tournaments!");
-                    //tournyFunctionCall();
+                    int tournamentsNumSelection = 0;
+
+                    Scanner tournamentsInput = new Scanner( System.in );
+                    while (tournamentsNumSelection != 3){
+                        System.out.println("Tournaments menu!");
+                        System.out.println("1. All tournaments");
+                        System.out.println("2. Tournament search");
+                        System.out.println("3. Return");
+                        tournamentsNumSelection = input.nextInt();
+                        if (tournamentsNumSelection == 1) {
+                            //return all users stored in the database
+
+                        } else if (tournamentsNumSelection == 2) {
+                             String temporaryInput;
+                            System.out.println("Enter name of member you are requesting info for: ");
+                            temporaryInput = tournamentsInput.next();
+                            //search database for a member with the name of <temporaryInput> and return all info
+
+                        } else if (tournamentsNumSelection == 3) {
+                            mainMenu();
+                        } else {
+                            System.out.println("Sorry wrong input!");
+                        }
+                    }
                     break;
                 case 3:
-                    System.out.println("Help!");
-                    //helpFunctionCall();
-                    break;
-                case 4:
-                    System.exit(0);
+                    mainMenu();
                 default:
-                    System.out.println("incorrect input please try again");
+                    System.out.println("Incorrect input please try again");
                     System.out.println("");
-                    mainMenuCall();
+
             }
 
     }
