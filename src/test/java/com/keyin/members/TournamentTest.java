3adf20f1f266b575782f03b35e971e0375d6ef29
Jacob Pritchett <95259685+Jakepritchett02@users.noreply.github.com>
2022-10-30 21:01:50 -0230

    Update on tournament class test

--- src/test/java/com/keyin/members/TournamentTest.java
+++ src/test/java/com/keyin/members/TournamentTest.java
@@ -1,3 +1,4 @@
+// Worked on by Jacob Pritchett
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
