9e5b9aaf37913400d99c3639357cf9a41042bb77
NoahG43 <95226957+NoahG43@users.noreply.github.com>
2022-10-30 16:36:50 -0230

    Added back Tournament Name, Remade Database and Manager file, Database methods return null for now

--- src/main/java/com/keyin/data/Database.java
+++ src/main/java/com/keyin/data/Database.java
@@ -1,23 +1,47 @@
 // Database file
 
 package com.keyin.data;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import com.keyin.members.Member;
 import com.keyin.members.Tournament;
 public class Database {
+    HashMap<String, Member> member = new HashMap<String, Member>();
+    HashMap<String, Tournament> tournament = new HashMap<String, Tournament>();
     public Member getMemberByName(String name){
-        return null;
+        return member.get(name);
     }
 
     public List<Member> getMemberByTournamentName(Member member){
-        return null;
+        List<Member> members = getAllMembers();
+
+        List<Member> membersToReturn = new ArrayList<Member>();
+
+        for (Member m : members) {
+            if (m.getMembers().getName().equals(member.getName())) {
+            membersToReturn.add(m);
+        }
+    }
+
+    return membersToReturn;
     }
 
     public Tournament getTournamentByName(String name){
-        return null;
+        return tournament.get(name);
     }
 
     public List<Tournament> getTournamentByMemberName(Tournament tournament){
-        return null;
+        List<Tournament> tournaments = getAllTournaments();
+
+        List<Tournament> tournamentsToReturn = new ArrayList<Tournament>();
+
+        for (Tournament t : tournaments) {
+            if (t.getTourName().getMembers().equals(tournament.getMembers())) {
+                tournamentsToReturn.add(t);
+            }
+        }
+
+        return tournamentsToReturn;
     }
 }
